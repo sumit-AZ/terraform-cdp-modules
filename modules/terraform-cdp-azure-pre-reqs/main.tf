4c5d4402dc032ab414dcb8e40962338937e9bf82
sumit-AZ <93200150+sumit-AZ@users.noreply.github.com>
2025-10-31 14:12:22 +0530

    Update main.tf

--- modules/terraform-cdp-azure-pre-reqs/main.tf
+++ modules/terraform-cdp-azure-pre-reqs/main.tf
@@ -16,20 +16,19 @@
 # TODO: Support case where network RG exists but CDP RG doesn't
 
 module "azure_cdp_rmgp" {
-
   source = "../terraform-azure-resource-group"
 
-  create_resource_group = (var.create_resource_group && var.create_vnet)
+  create_resource_group = var.create_resource_group
 
   # Variables required when creating RG
-  resourcegroup_name = (var.create_resource_group && var.create_vnet) ? local.cdp_resourcegroup_name : null
-  azure_region       = (var.create_resource_group && var.create_vnet) ? var.azure_region : null
-  tags               = (var.create_resource_group && var.create_vnet) ? var.env_tags : null
+  resourcegroup_name = var.create_resource_group ? local.cdp_resourcegroup_name : null
+  azure_region       = var.create_resource_group ? var.azure_region : null
+  tags               = var.create_resource_group ? var.env_tags : null
 
   # Variables required when using pre-existing RG
-  existing_resource_group_name = (var.create_resource_group && var.create_vnet) ? null : var.cdp_resourcegroup_name
-
+  existing_resource_group_name = var.create_resource_group ? null : var.cdp_resourcegroup_name
 }
+
 
 module "azure_network_rmgp" {
 
